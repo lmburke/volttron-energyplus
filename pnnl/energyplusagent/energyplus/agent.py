61e25317f3638cd80e46e32be35000a010d7f200
Robert Lutes <robert.lutes@pnnl.gov>
2017-02-08 13:00:20 -0800

    Minor fixes and add method to continue simulation without application interaction.

--- pnnl/energyplusagent/energyplus/agent.py
+++ pnnl/energyplusagent/energyplus/agent.py
@@ -1,56 +1,58 @@
 # -*- coding: utf-8 -*- {{{
 # vim: set fenc=utf-8 ft=python sw=4 ts=4 sts=4 et:
-#
-# Copyright (c) 2016, Battelle Memorial Institute
+
+# Copyright (c) 2015, Battelle Memorial Institute
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
+# modification, are permitted provided that the following conditions
+# are met:
 #
-# 1. Redistributions of source code must retain the above copyright notice, this
-#    list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-#    this list of conditions and the following disclaimer in the documentation
-#    and/or other materials provided with the distribution.
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-# The views and conclusions contained in the software and documentation are those
-# of the authors and should not be interpreted as representing official policies,
-# either expressed or implied, of the FreeBSD Project.
+# The views and conclusions contained in the software and documentation
+# are those of the authors and should not be interpreted as representing
+# official policies, either expressed or implied, of the FreeBSD
+# Project.
 #
-
 # This material was prepared as an account of work sponsored by an
 # agency of the United States Government.  Neither the United States
 # Government nor the United States Department of Energy, nor Battelle,
-# nor any of their employees, nor any jurisdiction or organization
-# that has cooperated in the development of these materials, makes
-# any warranty, express or implied, or assumes any legal liability
-# or responsibility for the accuracy, completeness, or usefulness or
-# any information, apparatus, product, software, or process disclosed,
-# or represents that its use would not infringe privately owned rights.
+# nor any of their employees, nor any jurisdiction or organization that
+# has cooperated in the development of these materials, makes any
+# warranty, express or implied, or assumes any legal liability or
+# responsibility for the accuracy, completeness, or usefulness or any
+# information, apparatus, product, software, or process disclosed, or
+# represents that its use would not infringe privately owned rights.
 #
 # Reference herein to any specific commercial product, process, or
-# service by trade name, trademark, manufacturer, or otherwise does
-# not necessarily constitute or imply its endorsement, recommendation,
-# r favoring by the United States Government or any agency thereof,
-# or Battelle Memorial Institute. The views and opinions of authors
+# service by trade name, trademark, manufacturer, or otherwise does not
+# necessarily constitute or imply its endorsement, recommendation, or
+# favoring by the United States Government or any agency thereof, or
+# Battelle Memorial Institute. The views and opinions of authors
 # expressed herein do not necessarily state or reflect those of the
 # United States Government or any agency thereof.
 #
 # PACIFIC NORTHWEST NATIONAL LABORATORY
 # operated by BATTELLE for the UNITED STATES DEPARTMENT OF ENERGY
 # under Contract DE-AC05-76RL01830
-
 #}}}
 
 from __future__ import absolute_import
@@ -66,7 +68,7 @@ from gevent import monkey
 monkey.patch_socket()
 from volttron.platform.agent import utils
 from volttron.platform.vip.agent import Core, RPC
-from pnnl.pubsubagent.pubsub.agent import SynchronizingPubSubAgent
+from pnnl.platform.pubsub.agent import SynchronizingPubSubAgent
 
 
 utils.setup_logging()
@@ -109,6 +111,7 @@ class EnergyPlusAgent(SynchronizingPubSubAgent):
     @Core.receiver('onstart')
     def start(self, sender, **kwargs):
         self.subscribe()
+        self.clearLastUpdate()
         self.startSocketServer()
         self.startSimulation()
 
@@ -145,9 +148,9 @@ class EnergyPlusAgent(SynchronizingPubSubAgent):
         self.writePortFile(os.path.join(modelDir,'socket.cfg'))
         self.writeVariableFile(os.path.join(modelDir,'variables.cfg'))
         if (self.version >= 8.4):
-            cmdStr = "cd %s; export BCVTB_HOME=\"%s\"; energyplus -w \"%s\" -r \"%s\"" % (modelDir, bcvtbDir, weatherPath, modelPath)
+            cmdStr = "cd %s; export BCVTB_HOME=%s; energyplus -w %s -r %s" % (modelDir, bcvtbDir, weatherPath, modelPath)
         else:
-            cmdStr = "export BCVTB_HOME=\"%s\"; runenergyplus \"%s\" \"%s\"" % (bcvtbDir, modelPath, weatherPath)
+            cmdStr = "export BCVTB_HOME=%s; runenergyplus %s %s" % (bcvtbDir, modelPath, weatherPath)
         log.debug('Running: %s', cmdStr)
         self.simulation = subprocess.Popen(cmdStr, shell=True)
     
@@ -205,7 +208,7 @@ class EnergyPlusAgent(SynchronizingPubSubAgent):
     def exit(self, mssg):
         self.stop()
         log.error(mssg)
-             
+      
 
     def stop(self):
         if self.socketServer:
@@ -290,8 +293,8 @@ class EnergyPlusAgent(SynchronizingPubSubAgent):
         """
         log.debug(requester_id + " canceled " + task_id)
         result = {'result':SUCCESS,
-                   'data': {},
-                   'info': ''}
+                  'data': {},
+                  'info': ''}
         return result   
         
         
@@ -310,7 +313,7 @@ class EnergyPlusAgent(SynchronizingPubSubAgent):
         :rtype: any base python type
          
         """
-        obj = self.getBestMatch(topic)
+        obj = self.findBestMatch(topic)
         if obj is not None: # we have an exact match to the  <device_name topic>/<point name>, so return the first value
             return obj.get('value', None)
         return None
@@ -338,9 +341,7 @@ class EnergyPlusAgent(SynchronizingPubSubAgent):
         topic = topic.strip('/')
         log.debug("Attempting to write "+topic+" with value: "+str(value))
         result = self.updateTopicRpc(requester_id, topic, value)
-        log.debug("Writing: {topic} : {value} {result}".format(topic=topic,
-                                          value=value,
-                                          result=result))
+        log.debug("Writing: {topic} : {value} {result}".format(topic=topic, value=value, result=result))
         if result==SUCCESS:
             return value;
         else:
@@ -362,7 +363,7 @@ class EnergyPlusAgent(SynchronizingPubSubAgent):
         :type requester_id: str
          
         """
-        obj = self.getGetBestMatch(topic)
+        obj = self.findBestMatch(topic)
         if obj and obj.has_key('default'):
             value = obj.get('default')
             log.debug("Reverting topic "+topic+" to "+str(value))
@@ -408,7 +409,19 @@ class EnergyPlusAgent(SynchronizingPubSubAgent):
             return SUCCESS
         return FAILURE
              
-             
+    def advance_simulation(self, peer, sender, bus, topic, headers, message):
+        log.debug('Advancing simulation.')
+        for obj in self.input().itervalues():
+            log.debug('ADVANCE: {}'.format(obj))
+            if (obj.has_key('blocking') and obj.get('blocking')) or not obj.has_key('blocking'):
+                if obj.has_key('last_update'):
+                    if obj.get('last_update') is None:
+                        set_topic = obj['topic']  + '/' + obj['field']
+                        value = obj['value'] if obj.has_key('value') else obj['default']
+                        self.updateTopicRpc('ahp', set_topic, value)
+        return
+                        
+         
     def onUpdateTopicRpc(self, requester_id, topic, value):
         self.updateComplete()
         
@@ -501,3 +514,4 @@ def main(argv=sys.argv):
 if __name__ == '__main__':
     # Entry point for script
     sys.exit(main())
+
